4b04ab56405db10c2c3c23c6aca87b9e2d3700c9
Tim <BerkyT115@yandex.ru>
2025-01-27 01:24:28 +0300

    added once mode

--- src/tasks/runner.py
+++ src/tasks/runner.py
@@ -6,22 +6,31 @@ from src.schedule import stop_event
 from src.services.log import logger
 
 
-def task_runner(interval: float = 60):
+def task_runner(interval: float = 60, once: bool = False):
     sleep_time = interval
 
     def task(func: Callable):
+        async def _once_call(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except Exception as e:
+                logger.error(f"Error in task '{func.__name__}': {e}")
+
+        async def _loop_call(*args, **kwargs):
+            while not stop_event.is_set():
+                _result = await _once_call(*args, **kwargs)
+                await asyncio.sleep(sleep_time)
+
+            return _result
+
         @wraps(func)
         async def wrapper(*args, **kwargs):
             logger.info(f"Starting task '{func.__name__}'")
-            result = None
 
-            while not stop_event.is_set():
-                try:
-                    result = await func(*args, **kwargs)
-                except Exception as e:
-                    logger.error(f"Error in task '{func.__name__}': {e}")
-
-                await asyncio.sleep(sleep_time)
+            if once:
+                result = await _once_call(*args, **kwargs)
+            else:
+                result = await _loop_call(*args, **kwargs)
 
             logger.info(f"Finishing task '{func.__name__}'")
             return result
